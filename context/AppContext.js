505e9dc192b00b676c96aa82d9170f42521d9096
🜲 PaPaGuy Codes <175421249+papaguycodes@users.noreply.github.com>
2024-11-18 15:14:54 +0100

    Create AppContext.js

---
+++ context/AppContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContext, AppProvider };
+
+
+<!-- // 💡✌🏾 In the End, He made them Open-Source 💜❤️ // -->
