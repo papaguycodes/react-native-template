c60208c6c6c7b2a54148438b7a3d451674a12f35
🜲 PaPaGuy Codes <175421249+papaguycodes@users.noreply.github.com>
2024-11-18 15:15:34 +0100

    Create Button.js

---
+++ components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const CustomButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#2E062B',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
+
+export default CustomButton;
